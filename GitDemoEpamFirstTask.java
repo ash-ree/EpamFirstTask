f8b567382ddd1fb07f5b2495479ad6badc602e8e
ash-ree <34602172+ash-ree@users.noreply.github.com>
2020-07-26 12:44:15 +0530

    Add files via upload

---
+++ GitDemoEpamFirstTask.java
@@ -0,0 +1,10 @@
+/*
+Create a git account, create a repository , upload a demo file in the repository.
+*/
+import java.util.*;
+public class GitDemoEpamFirstTask{
+    public static void main(String args[]){
+        System.out.println("This is git demo");
+        System.out.println("First Epam Task");
+    }
+}
